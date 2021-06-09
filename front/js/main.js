bb455ae7b5069d8fa19fd68eb370e5369d09f941
Svetlana Petrosyan <petrosyansvetlana7@gmail.com>
2021-06-09 21:24:47 +0400

    asd

--- front/js/main.js
+++ front/js/main.js
@@ -17,6 +17,73 @@ $(".accordion__title").on("click", function () {
     }
 });
 
+  var bigimage = $("#big");
+  var thumbs = $("#thumbs");
+  //var totalslides = 10;
+  var syncedSecondary = true;
+
+  bigimage
+    .owlCarousel({
+    items: 1,
+    slideSpeed: 2000,
+    nav: false,
+    
+    // autoplay: true,
+    dots: false,
+    // loop: true,
+    responsiveRefreshRate: 200,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>'
+    ]
+  })
+    .on("changed.owl.carousel", syncPosition);
+
+  thumbs
+    .on("initialized.owl.carousel", function() {
+    thumbs
+      .find(".owl-item")
+      .eq(0)
+      .addClass("current");
+  })
+    .owlCarousel({
+    items: 7,
+    dots: false,
+    // loop:true,
+    nav: false,
+    navText: [
+     '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>'
+    ],
+    smartSpeed: 200,
+    slideSpeed: 500,
+    slideBy: 1,
+    responsiveRefreshRate: 100
+  })
+     
+$('.modal.thankYou .message button').on('click', function() {
+    $('.modal').hide()
+})
+  function syncPosition(el) {
+    //if loop is set to false, then you have to uncomment the next line
+    //var current = el.item.index;
+
+    //to disable loop, comment this block
+    console.log(el);
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+ 
+     
+  }
+
+  
+
+  thumbs.on("click", ".owl-item", function(e) {
+    e.preventDefault();
+    var number = $(this).index();
+    bigimage.data("owl.carousel").to(number, 300, true);
+  });
+ 
    
 $(".owl-carousel.regions ").owlCarousel({
     
